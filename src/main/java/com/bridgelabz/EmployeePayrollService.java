5150e6e23e4256abb32853f2b7d2d726e8e877c8
NagrajPanchal <nagrajsubhashpanchal@gmail.com>
2022-11-24 14:43:22 +0530

    Added : JDBC Coonection

--- src/main/java/com/bridgelabz/EmployeePayrollService.java
+++ src/main/java/com/bridgelabz/EmployeePayrollService.java
@@ -1,2 +1,22 @@
-package com.bridgelabz;public class EmployeePayrollService {
+package com.bridgelabz;
+
+import java.sql.*;
+
+public class EmployeePayrollService
+{
+    public static void main(String[] args)
+    {
+        String url = "jdbc:mysql://127.0.0.1:3306/employee_payroll"; // table details
+        String username = "root"; // MySQL credentials
+        String password = "root";
+        try
+        {
+            Class.forName("com.mysql.cj.jdbc.Driver"); // Driver name
+            Connection connection = DriverManager.getConnection(url, username, password);
+            System.out.println("Connection Established successfully "+connection);
+        } catch (SQLException | ClassNotFoundException e)
+        {
+            System.out.println("Catch Block");
+        }
+    }
 }
