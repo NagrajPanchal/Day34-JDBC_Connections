07c10d769c8ea79a6d53677e6d4741567ae5c358
NagrajPanchal <nagrajsubhashpanchal@gmail.com>
2022-11-24 18:18:05 +0530

    Added : Aggregate Functions and group by

--- src/main/java/com/bridgelabz/EmployeePayrollService.java
+++ src/main/java/com/bridgelabz/EmployeePayrollService.java
@@ -9,28 +9,28 @@ public class EmployeePayrollService
         String url = "jdbc:mysql://127.0.0.1:3306/employee_payroll"; // table details
         String username = "root"; // MySQL credentials
         String password = "root";
+        String query = "Select min(id),max(id),sum(id),avg(id),count(id)\n" +
+                "from employee where gender = \"male\"\n" +
+                "group by gender;";
         try {
             Class.forName("com.mysql.cj.jdbc.Driver"); // Driver name
             Connection con = DriverManager.getConnection(url, username, password);
             System.out.println("Connection Established successfully");
-            PreparedStatement st = con.prepareStatement("Select min(id),max(id),avg(id),count(id),sum(id) \n" +
-                    "from employee where gender = ?" +
-                    "group by gender;");
-            st.setString(1,"male");
-            boolean result = st.execute();
+            Statement st = con.createStatement();
+            boolean result = st.execute(query);
             if (result) {
                 ResultSet rs = st.getResultSet();
                 while (rs.next()) {
-                    int id = rs.getInt("id"); // Retrieve name from db
-                    System.out.println(id); // Print result on console
-                    String name = rs.getString("name"); // Retrieve name from db
-                    System.out.println(name); // Print result on console
-                    String gender = rs.getString("gender"); // Retrieve name from db
-                    System.out.println(gender); // Print result on console
-                    String city = rs.getString("city"); // Retrieve name from db
-                    System.out.println(city); // Print result on console
-                    String email = rs.getString("email"); // Retrieve name from db
-                    System.out.println(email); // Print result on console
+                    int minId = rs.getInt("min(id)");
+                    System.out.println("Min : "+minId);
+                    int maxId = rs.getInt("max(id)");
+                    System.out.println("Max : "+maxId);
+                    int sumId = rs.getInt("sum(id)");
+                    System.out.println("Sum : "+sumId);
+                    int avgId = rs.getInt("avg(id)");
+                    System.out.println("Avg : "+avgId);
+                    int countId = rs.getInt("count(id)");
+                    System.out.println("Count : "+countId);
                 }
             } else {
                 int rowCount = st.getUpdateCount();
@@ -40,7 +40,7 @@ public class EmployeePayrollService
             con.close(); // close connection
             System.out.println("Connection Closed....");
         } catch (SQLException | ClassNotFoundException e) {
-            System.out.println("Catch Block");
+            System.out.println(e);
         }
     }
 }
